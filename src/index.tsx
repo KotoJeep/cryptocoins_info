66102d5d597ee0b0183b3aa12f5b6e752de12f28
ВОЛКОВ АНТОН АЛЕКСАНДРОВИЧ <omaigader@gmail.com>
2023-02-16 15:14:58 +0700

    feat: add loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.scss';
+import {Loader} from "./components/Loader/Loader";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div />
+    <div>
+        <Loader/>
+    </div>
   </React.StrictMode>
 );
